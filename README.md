8877008a3d638b839ba57763f2db0d15a6100868
migitera <migiterayu@crunchtimer.jp>
2020-08-25 19:27:10 +0900

    add: api

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # api-bio-qiita
 
-バック用リポジトリ
+bio-qiitaバック
 
-dockerのみ設定してある。
+単純なAPIまで作成済み。
 
-まずlaravelプロジェクトを作成することが必要。
+API仕様
+
+```
+Method    URI                    機能
+------------------------------------------
+GET|HEAD  api/articles           一覧取得
+POST      api/articles           投稿
+GET|HEAD  api/articles/{article} 詳細取得
+PUT|PATCH api/articles/{article} 更新
+DELETE    api/articles/{article} 削除
+```
+
+とりあえず動いているかどうか確かめるには
+
+Talend API TesterでGET http://localhost:80/api/articles
+
+次のバックのタスクはシーディング実装。
---
+++ backend/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 投稿を作成日時の新しい順でペジネーションした結果を返す
+        return Article::latest()->paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // リクエストパラメータから取得した値をもとにArticleを作成する
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        // Eloquentオブジェクトを返す
+        return $article;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        // リクエストパラメータから取得した値をもとにPostを更新する
+        $article->update($request->all());
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+        // Eloquentオブジェクトを削除する
+        $deleted = $article->delete();
+        return compact('deleted');
+    }
+}
